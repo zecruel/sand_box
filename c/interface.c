dc0e71d447807d42f7df2e32a8614ee19ac82531
Ezequiel <zecruel@ig.com.br>
2017-11-13 16:59:58 -0200

    Add files via upload

--- c/interface.c
+++ c/interface.c
@@ -30,7 +30,7 @@
 #include "gui.h"
 #include "images.c"
 
-enum theme {THEME_BLACK, THEME_WHITE, THEME_RED, THEME_BLUE, THEME_DARK};
+enum theme {THEME_BLACK, THEME_WHITE, THEME_RED, THEME_BLUE, THEME_DARK, THEME_ZE};
 
 void
 set_style(struct nk_context *ctx, enum theme theme)
@@ -155,6 +155,36 @@ set_style(struct nk_context *ctx, enum theme theme)
         table[NK_COLOR_SCROLLBAR_CURSOR_HOVER] = nk_rgba(53, 88, 116, 255);
         table[NK_COLOR_SCROLLBAR_CURSOR_ACTIVE] = nk_rgba(58, 93, 121, 255);
         table[NK_COLOR_TAB_HEADER] = nk_rgba(48, 83, 111, 255);
+        nk_style_from_table(ctx, table);
+    } else if (theme == THEME_ZE) {
+        table[NK_COLOR_TEXT] = nk_rgba(210, 210, 210, 255);
+        table[NK_COLOR_WINDOW] = nk_rgba(57, 71, 58, 215);
+        table[NK_COLOR_HEADER] = nk_rgba(52, 57, 52, 220);
+        table[NK_COLOR_BORDER] = nk_rgba(46, 46, 46, 255);
+        table[NK_COLOR_BUTTON] = nk_rgba(48, 112, 54, 255);
+        table[NK_COLOR_BUTTON_HOVER] = nk_rgba(71, 161, 80, 255);
+        table[NK_COLOR_BUTTON_ACTIVE] = nk_rgba(89, 201, 100, 255);
+        table[NK_COLOR_TOGGLE] = nk_rgba(50, 61, 50, 255);
+        table[NK_COLOR_TOGGLE_HOVER] = nk_rgba(46, 57, 46, 255);
+        table[NK_COLOR_TOGGLE_CURSOR] = nk_rgba(48, 112, 54, 255);
+        table[NK_COLOR_SELECT] = nk_rgba(58, 67, 57, 255);
+        table[NK_COLOR_SELECT_ACTIVE] = nk_rgba(48, 112, 54, 255);
+        table[NK_COLOR_SLIDER] = nk_rgba(50, 61, 50, 255);
+        table[NK_COLOR_SLIDER_CURSOR] = nk_rgba(48, 112, 54, 245);
+        table[NK_COLOR_SLIDER_CURSOR_HOVER] = nk_rgba(59, 115, 53, 255);
+        table[NK_COLOR_SLIDER_CURSOR_ACTIVE] = nk_rgba(71, 161, 80, 255);
+        table[NK_COLOR_PROPERTY] = nk_rgba(50, 61, 50, 255);
+        table[NK_COLOR_EDIT] = nk_rgba(50, 61, 50, 225);
+        table[NK_COLOR_EDIT_CURSOR] = nk_rgba(210, 210, 210, 255);
+        table[NK_COLOR_COMBO] = nk_rgba(50, 61, 50, 255);
+        table[NK_COLOR_CHART] = nk_rgba(50, 61, 50, 255);
+        table[NK_COLOR_CHART_COLOR] = nk_rgba(48, 112, 54, 255);
+        table[NK_COLOR_CHART_COLOR_HIGHLIGHT] = nk_rgba(255, 0, 0, 255);
+        table[NK_COLOR_SCROLLBAR] = nk_rgba(50, 61, 50, 255);
+        table[NK_COLOR_SCROLLBAR_CURSOR] = nk_rgba(48, 112, 54, 255);
+        table[NK_COLOR_SCROLLBAR_CURSOR_HOVER] = nk_rgba(59, 115, 53, 255);
+        table[NK_COLOR_SCROLLBAR_CURSOR_ACTIVE] = nk_rgba(71, 161, 80, 255);
+        table[NK_COLOR_TAB_HEADER] = nk_rgba(48, 112, 54, 255);
         nk_style_from_table(ctx, table);
     } else {
         nk_style_default(ctx);
@@ -412,7 +442,7 @@ int main(int argc, char** argv){
 	shape *shx_font = shx_font_open("txt.shx");
 	gui_obj *gui = nk_sdl_init(shx_font);
 	
-	set_style(gui->ctx, THEME_DARK);
+	set_style(gui->ctx, THEME_ZE);
 	
 	gui->ctx->style.edit.padding = nk_vec2(4, -6);
 	
