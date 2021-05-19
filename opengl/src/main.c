db8eeed6f9f60dd5aecfbb7362d7082db2487de9
Ezequiel <ezeq.cruel@gmail.com>
2021-05-19 15:58:55 -0300

    Add files via upload

--- opengl/src/main.c
+++ opengl/src/main.c
@@ -10,6 +10,7 @@
 #define MAX_TRIANG 500
 #define TOLERANCE 1e-6
 #define MAX_SCAN_LINES 1000
+#define MAX_P_NODES 1000
 
 #define GLSL(src) "#version 150 core\n" #src
 
@@ -67,6 +68,18 @@ struct ogl {
 struct edge {
 	int x0, y0, x1, y1;
 };
+
+struct p_node {
+	int value;
+	struct edge * edge;
+};
+
+/* comparation function for qsort */
+int cmp_node (const void * a, const void * b) {
+	struct p_node *aa, *bb;
+	aa = (struct p_node *) a; bb = (struct p_node *) b;
+	return (aa->value - bb->value);
+}
 
 int line_gl (struct ogl *gl_ctx, int p0[2], int p1[2], int thick){
 	if (!gl_ctx) return 0;
@@ -165,6 +178,96 @@ int line_gl (struct ogl *gl_ctx, int p0[2], int p1[2], int thick){
 	return 1;
 }
 
+int trap_gl (struct ogl *gl_ctx, int tl[2], int bl[2], int tr[2], int br[2]){
+	if (!gl_ctx) return 0;
+	if (!gl_ctx->verts) return 0;
+	if (!gl_ctx->elems) return 0;
+		
+	int j = gl_ctx->vert_count;
+	
+	float flip_y = (gl_ctx->flip_y) ? -1.0 : 1.0;
+	
+	gl_ctx->verts[j].pos[0] = ((float) bl[0] / gl_ctx->win_w) * 2.0 - 1.0;
+	gl_ctx->verts[j].pos[1] = flip_y * (((float) bl[1] / gl_ctx->win_h) * 2.0 - 1.0);
+	gl_ctx->verts[j].pos[2] = 0.0;
+	gl_ctx->verts[j].col[0] = gl_ctx->fg[0];
+	gl_ctx->verts[j].col[1] = gl_ctx->fg[1];
+	gl_ctx->verts[j].col[2] = gl_ctx->fg[2];
+	gl_ctx->verts[j].col[3] = gl_ctx->fg[3];
+	gl_ctx->verts[j].uv[0]= 0.0;
+	gl_ctx->verts[j].uv[1]= (float)(gl_ctx->flip_y);
+	gl_ctx->vert_count ++;
+	
+	j = gl_ctx->vert_count;
+	
+	gl_ctx->verts[j].pos[0] = ((float) tl[0] / gl_ctx->win_w) * 2.0 - 1.0;
+	gl_ctx->verts[j].pos[1] = flip_y * (((float) tl[1] / gl_ctx->win_h) * 2.0 - 1.0);
+	gl_ctx->verts[j].pos[2] = 0.0;
+	gl_ctx->verts[j].col[0] = gl_ctx->fg[0];
+	gl_ctx->verts[j].col[1] = gl_ctx->fg[1];
+	gl_ctx->verts[j].col[2] = gl_ctx->fg[2];
+	gl_ctx->verts[j].col[3] = gl_ctx->fg[3];
+	gl_ctx->verts[j].uv[0]= 0.0;
+	gl_ctx->verts[j].uv[1]= (float)(1 - gl_ctx->flip_y);
+	gl_ctx->vert_count ++;
+	
+	j = gl_ctx->vert_count;
+	
+	gl_ctx->verts[j].pos[0] = ((float) br[0] / gl_ctx->win_w) * 2.0 - 1.0;
+	gl_ctx->verts[j].pos[1] = flip_y * (((float) br[1] / gl_ctx->win_h) * 2.0 - 1.0);
+	gl_ctx->verts[j].pos[2] = 0.0;
+	gl_ctx->verts[j].col[0] = gl_ctx->fg[0];
+	gl_ctx->verts[j].col[1] = gl_ctx->fg[1];
+	gl_ctx->verts[j].col[2] = gl_ctx->fg[2];
+	gl_ctx->verts[j].col[3] = gl_ctx->fg[3];
+	gl_ctx->verts[j].uv[0]= 1.0;
+	gl_ctx->verts[j].uv[1]= (float)(gl_ctx->flip_y);
+	gl_ctx->vert_count ++;
+	
+	j = gl_ctx->vert_count;
+	
+	gl_ctx->verts[j].pos[0] = ((float) tr[0] / gl_ctx->win_w) * 2.0 - 1.0;
+	gl_ctx->verts[j].pos[1] = flip_y * (((float) tr[1] / gl_ctx->win_h) * 2.0 - 1.0);
+	gl_ctx->verts[j].pos[2] = 0.0;
+	gl_ctx->verts[j].col[0] = gl_ctx->fg[0];
+	gl_ctx->verts[j].col[1] = gl_ctx->fg[1];
+	gl_ctx->verts[j].col[2] = gl_ctx->fg[2];
+	gl_ctx->verts[j].col[3] = gl_ctx->fg[3];
+	gl_ctx->verts[j].uv[0]= 1.0;
+	gl_ctx->verts[j].uv[1]= (float)(1 - gl_ctx->flip_y);
+	gl_ctx->vert_count ++;
+	
+	j = gl_ctx->elem_count * 3;
+	
+	gl_ctx->elems[j] = gl_ctx->vert_count - 4;
+	gl_ctx->elems[j+1] = gl_ctx->vert_count - 3;
+	gl_ctx->elems[j+2] = gl_ctx->vert_count - 2;
+	gl_ctx->elems[j+3] = gl_ctx->vert_count - 3;
+	gl_ctx->elems[j+4] = gl_ctx->vert_count - 2;
+	gl_ctx->elems[j+5] = gl_ctx->vert_count - 1;
+	
+	gl_ctx->elem_count+= 2;
+	
+	return 1;
+}
+
+// To find orientation of ordered triplet (p, q, r).
+// The function returns following values
+// 0 --> p, q and r are colinear
+// 1 --> Clockwise
+// 2 --> Counterclockwise
+int orientation(int p[2], int q[2], int r[2])
+{
+    // See https://www.geeksforgeeks.org/orientation-3-ordered-points/
+    // for details of below formula.
+    int val = (q[1] - p[1]) * (r[0] - q[0]) -
+              (q[0] - p[0]) * (r[1] - q[1]);
+  
+    if (val == 0) return 0;  // colinear
+  
+    return (val > 0)? 1: 2; // clock or counterclock wise
+}
+
 int polygon_gl (struct ogl *gl_ctx, int n, struct edge edges[]){
 	if (!gl_ctx) return 0;
 	if (!gl_ctx->verts) return 0;
@@ -173,8 +276,11 @@ int polygon_gl (struct ogl *gl_ctx, int n, struct edge edges[]){
 	if (2 * n > MAX_SCAN_LINES) return 0;
 	
 	int scan_lines[MAX_SCAN_LINES];
+	struct p_node nodes1[MAX_P_NODES], nodes2[MAX_P_NODES];
 	
-	int i = 0, scan_count = 0;
+	int i = 0, j = 0, k = 0, scan_count = 0, n1 = 0, n2 = 0;
+	int trap_x[MAX_P_NODES][4];
+	int trap_count = 0;
 	
 	for (i = 0; i < n; i++){
 		scan_lines[scan_count] = edges[i].y0;
@@ -186,18 +292,163 @@ int polygon_gl (struct ogl *gl_ctx, int n, struct edge edges[]){
 	
 	scan_count = unique (scan_count, scan_lines);
 	
-	for (i = 0; i < scan_count; i++){
-		printf("%d\n", scan_lines[i]);
+	for (i = 1; i < scan_count; i++){
+		
+		n1 = 0;
+		n2 = 0;
+		trap_count = 0;
+		
+		for(j = 0; j < n; j++){
+			if (((edges[j].y0 <= scan_lines[i - 1] && edges[j].y1 >= scan_lines[i - 1]) || 
+			(edges[j].y1 <= scan_lines[i - 1] && edges[j].y0 >= scan_lines[i - 1])) &&
+			((edges[j].y0 <= scan_lines[i] && edges[j].y1 >= scan_lines[i]) || 
+			(edges[j].y1 <= scan_lines[i ] && edges[j].y0 >= scan_lines[i]))){
+				if (edges[j].y0 == scan_lines[i - 1]) {
+					nodes1[n1].value = edges[j].x0;
+					nodes1[n1].edge = &edges[j];
+					n1++;
+				}
+				else if (edges[j].y1 == scan_lines[i-1]) {
+					nodes1[n1].value = edges[j].x1;
+					nodes1[n1].edge = &edges[j];
+					n1++;
+				}
+				else {
+					nodes1[n1].value = (int) round(edges[j].x1 + (double) (scan_lines[i - 1] - edges[j].y1)/(edges[j].y0 - edges[j].y1)*(edges[j].x0 - edges[j].x1));
+					nodes1[n1].edge = &edges[j];
+					n1++;
+				}
+				
+				if (edges[j].y0 == scan_lines[i]) {
+					nodes2[n2].value = edges[j].x0;
+					nodes2[n2].edge = &edges[j];
+					n2++;
+				}
+				else if (edges[j].y1 == scan_lines[i]) {
+					nodes2[n2].value = edges[j].x1;
+					nodes2[n2].edge = &edges[j];
+					n2++;
+				}
+				else {
+					nodes2[n2].value = (int) round(edges[j].x1 + (double) (scan_lines[i] - edges[j].y1)/(edges[j].y0 - edges[j].y1)*(edges[j].x0 - edges[j].x1));
+					nodes2[n2].edge = &edges[j];
+					n2++;
+				}
+			}
+		}
+		
+		qsort(nodes1, n1, sizeof(struct p_node), cmp_node);
+		qsort(nodes2, n2, sizeof(struct p_node), cmp_node);
+		
+		int bl = 0, br = n2 - 1;
+		int tl = 0, tr = n1 - 1;
+		int complete = 0;
+		int last_tl = nodes1[n1 - 1].value, last_tr = nodes1[0].value;
+		int last_bl = nodes2[n2 - 1].value, last_br = nodes2[0].value;
+		int valid_diag = 0;
+		
+		complete = 0;
+		trap_count = 0;
+		while (!complete){
+			int p1[2], p2[2], q1[2], q2[2], o1, o2, o3, o4;
+			valid_diag = 1;
+			for (j = 0; j  < n2 - 1; j++){
+				p1[0] = nodes2[j].edge->x0;
+				p1[1] = nodes2[j].edge->y0;
+				q1[0] = nodes2[j].edge->x1;
+				q1[1] = nodes2[j].edge->y1;
+				
+				p2[0] = nodes1[tl].value;
+				p2[1] = scan_lines[i-1];
+				q2[0] = nodes2[br].value;
+				q2[1] = scan_lines[i];
+				
+				o1 = orientation(p1, q1, p2);
+				o2 = orientation(p1, q1, q2);
+				o3 = orientation(p2, q2, p1);
+				o4 = orientation(p2, q2, q1);
+
+				// General case
+				if (o1 > 0 && o2 > 0 && o1 != o2 && o3 != o4){
+					if (j  >= n2 - 2) valid_diag = 0;
+					j = 0;
+					br--;
+				}
+			}
+			
+			if (valid_diag && (last_tl != nodes1[tl].value || last_br != nodes2[br].value)){
+				//printf("d(%d,%d) ", nodes1[tl].value, nodes2[br].value);
+				
+				trap_x[trap_count][0] = nodes1[tl].value;
+				trap_x[trap_count][3] = nodes2[br].value;
+				trap_count++;
+			}
+			
+			last_tl = nodes1[tl].value;
+			last_br = nodes2[br].value;
+			
+			if(nodes2[br].value == nodes2[n2 - 1].value) complete = 1;
+			
+			br = n2 - 1;
+			tl++;
+		}
+		
+		complete = 0;
+		trap_count = 0;
+		while (!complete){
+			int p1[2], p2[2], q1[2], q2[2], o1, o2, o3, o4;
+			valid_diag = 1;
+			for (j = 0; j  < n1 - 1; j++){
+				p1[0] = nodes1[j].edge->x0;
+				p1[1] = nodes1[j].edge->y0;
+				q1[0] = nodes1[j].edge->x1;
+				q1[1] = nodes1[j].edge->y1;
+				
+				p2[0] = nodes2[bl].value;
+				p2[1] = scan_lines[i];
+				q2[0] = nodes1[tr].value;
+				q2[1] = scan_lines[i-1];
+				
+				o1 = orientation(p1, q1, p2);
+				o2 = orientation(p1, q1, q2);
+				o3 = orientation(p2, q2, p1);
+				o4 = orientation(p2, q2, q1);
+
+				// General case
+				if (o1 > 0 && o2 > 0 && o1 != o2 && o3 != o4){
+					if (j  >= n1 - 2) valid_diag = 0;
+					j = 0;
+					tr--;
+				}
+			}
+			
+			if (valid_diag && (last_tr != nodes1[tr].value || last_bl != nodes2[bl].value)){
+				//printf("u(%d,%d) ", nodes2[bl].value, nodes1[tr].value);
+				
+				trap_x[trap_count][1] = nodes2[bl].value;
+				trap_x[trap_count][2] = nodes1[tr].value;
+				trap_count++;
+			}
+			
+			last_tr = nodes1[tr].value;
+			last_bl = nodes2[bl].value;
+			
+			if(nodes1[tr].value == nodes1[n1 - 1].value) complete = 1;
+			
+			tr = n1 - 1;
+			bl++;
+		}
+		
+		for (j = 0; j < trap_count; j++){
+			//int trap_gl (struct ogl *gl_ctx, int tl[2], int bl[2], int tr[2], int br[2]){
+			trap_gl (gl_ctx, (int[]){trap_x[j][0], scan_lines[i-1]},
+				(int[]){trap_x[j][1], scan_lines[i]},
+				(int[]){trap_x[j][2], scan_lines[i-1]},
+				(int[]){trap_x[j][3], scan_lines[i]});
+		}
+		
+		//printf("- y = %d\n", scan_lines[i]);
 	}
-	
-	/*if(((y0 < pix_y) && (y1 >= pix_y)) || 
-	((y1 < pix_y) && (y0 >= pix_y))){
-		/* find x coord of intersection and add to list 
-		node_x[nodes] = (int) round(x1 + (double) (pix_y - y1)/(y0 - y1)*(x0 - x1));
-		node_x[nodes] = (node_x[nodes] >= 0) ? node_x[nodes] : -1;
-		node_x[nodes] = (node_x[nodes] <= max_x) ? node_x[nodes] : max_x + 1;
-		nodes++;
-	}*/
 	
 	return 1;
 }
@@ -369,7 +620,7 @@ int main(int argc, char *argv[])
         gl_ctx.verts = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
         gl_ctx.elems = glMapBuffer(GL_ELEMENT_ARRAY_BUFFER, GL_WRITE_ONLY);
 	
-	polygon_gl (&gl_ctx, 13, test_edges);
+	//polygon_gl (&gl_ctx, 13, test_edges);
 	
 	int i = 0, j;
 	SDL_Event windowEvent;
@@ -413,6 +664,9 @@ int main(int argc, char *argv[])
 			
 			line_gl(&gl_ctx, p0, p1, 2);
 		}
+		
+		gl_ctx.fg[0] = 0; gl_ctx.fg[1] = 255; gl_ctx.fg[2] = 0; gl_ctx.fg[3] = 100;
+		polygon_gl (&gl_ctx, 13, test_edges);
 		
 		
 		
