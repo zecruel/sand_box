ef6dbd40f6de02209bc292441bfcde18d21270d3
Ezequiel <ezeq.cruel@gmail.com>
2024-06-13 10:28:19 -0300

    Add files via upload

--- webui-windows-gcc-x64/index.js
+++ webui-windows-gcc-x64/index.js
@@ -6,7 +6,7 @@ let changes = -1;
 let db_changes = 0;
 
 const cor_claro = [
-  '#eeeeee',
+  '#cccccc',
   '#ffccff',
   '#ffcccc',
   '#ffffcc',
@@ -16,13 +16,13 @@ const cor_claro = [
 ];
 
 const cor_escuro = [
-  '#eeeeee',
-  '#ffb0ff',
-  '#ffb0b0',
-  '#ffffb0',
-  '#b0ffb0',
-  '#b0ffff',
-  '#b0b0ff',
+  '#909090',
+  '#ff90ff',
+  '#ff9090',
+  '#ffff90',
+  '#90ff90',
+  '#90ffff',
+  '#9090ff',
 ];
   
 
@@ -60,21 +60,6 @@ const redo = function(instance, obj) {
   }
 };
 
-const table = jspreadsheet(document.getElementById('my-spreadsheet'), {
-    data:data,
-    columns:[
-        { title:'Model', width:300 },
-        { title:'Price', width:80 },
-        { title:'Model', width:100 }
-    ],
-    meta:{
-        A1: { myMeta: 'this is just a test', otherMetaInformation: 'other test' },
-        A2: { info: 'test' }
-    },
-    onchange: beforeChange,
-    onundo: undo,
-    onredo: redo,
-});
 
 const loaded = function(instance) {
   let comp_ant = "";
@@ -89,35 +74,75 @@ const loaded = function(instance) {
   let ini_modulo = 1;
   let ini_painel = 1;
   
-  let paineis = 0, unicos = 0;
+  let paineis = 0, unicos = 0, componentes = 0, modulos = 0;
   
   //agrupa as celulas repetidas
   if (comp_term_dados[0]) {
     painel_ant = comp_term_dados[0][0];
+    comp_ant = comp_term_dados[0][1];
     unico_ant = comp_term.getMeta('H1').id;
+    parte_ant = comp_term_dados[0][3];
+    modulo_ant = comp_term_dados[0][2];
   }
   let y = 1;
   let claro = cor_claro;
+  let cor_uni = claro[unicos % 7];
   
   for (let linha of comp_term_dados){
+    
+    
+    
+    
+    
     unico = comp_term.getMeta('H' + y).id;
+    
     if (painel_ant != linha[0]){
       if ((y - ini_painel) > 1) {
         comp_term.setMerge("A"+ini_painel, 1, y - ini_painel);
       }
       ini_painel = y;
       
-      const cel = comp_term.getCell("A"+y);
-      if ((paineis % 2) == 0){ 
-        cel.style.backgroundColor = "#ffb0b0";
-        claro = cor_claro;
-      }
-      else { 
-        cel.style.backgroundColor = "#ffcccc";
-        claro = cor_escuro;
-      }
+      let cor = claro[paineis % 7];
+      
+      const cel = comp_term.getCell("A" + y);
+      cel.style.backgroundColor = cor;
+      cel.classList.add('readonly');
+      cel.style.color = "#900";
+      
       paineis++;
       unicos = 0;
+      componentes = 0;
+      modulos = 0;
+    }
+    
+    if (painel_ant != linha[0] || comp_ant != linha[1]){
+      if ((y - ini_comp) > 1) {
+        comp_term.setMerge("B" + ini_comp, 1, y - ini_comp);
+      }
+      ini_comp = y;
+      
+      let cor = claro[componentes % 7];
+      
+      let cel = comp_term.getCell("B" + y);
+      cel.style.backgroundColor = cor;
+      cel.classList.add('readonly');
+      cel.style.color = "#900";
+      componentes++;
+    }
+    
+    if (painel_ant != linha[0] || comp_ant != linha[1] || modulo_ant != linha[2]){
+      if ((y - ini_modulo) > 1) {
+        comp_term.setMerge("C" + ini_modulo, 1, y - ini_modulo);
+      }
+      ini_modulo = y;
+      
+      let cor = claro[modulos % 7];
+      
+      let cel = comp_term.getCell("C" + y);
+      cel.style.backgroundColor = cor;
+      cel.classList.add('readonly');
+      cel.style.color = "#900";
+      modulos++;
     }
     
     //o ID unico eh o criterio para agrupar blocos e partes
@@ -128,25 +153,56 @@ const loaded = function(instance) {
         comp_term.setMerge("F"+ini_unico, 1, y - ini_unico);
       }
       ini_unico = y;
-      let cor = claro[unicos % 7];
+      cor_uni = claro[unicos % 7];
       
-      //if ((unicos % 2) == 0){ 
-        let cel = comp_term.getCell("D"+y);
-        cel.style.backgroundColor = cor;
-        cel = comp_term.getCell("E"+y);
-        cel.style.backgroundColor = cor;
-        cel = comp_term.getCell("F"+y);
-        cel.style.backgroundColor = cor;
-      //}
+      
+      let cel = comp_term.getCell("D"+y);
+      cel.style.backgroundColor = cor_uni;
+      if (linha[3] !== ""){
+        cel.classList.remove('readonly');
+        cel.style.color = "#000";
+      }
+      else {
+        cel.classList.add('readonly');
+        cel.style.color = "#900";
+      }
+      
+      cel = comp_term.getCell("E"+y);
+      cel.style.backgroundColor = cor_uni;
+      cel.classList.add('readonly');
+      cel.style.color = "#900";
+      
+      cel = comp_term.getCell("F"+y);
+      cel.style.backgroundColor = cor_uni;
+      cel.classList.add('readonly');
+      cel.style.color = "#900";
+      
       unicos++;
     }
     
-    //comp_ant = linha.componente;
+    let celula = comp_term.getCell("G" + y);
+    celula.style.backgroundColor = cor_uni;
+    celula.classList.add('readonly');
+    celula.style.color = "#900";
+    
+    celula = comp_term.getCell("H" + y);
+    celula.style.backgroundColor = cor_uni;
+    //celula.classList.add('readonly');
+    //celula.style.color = "#900";
+    
+    comp_ant = linha[1];
     unico_ant = unico;
     //tipo_ant = linha.tipo;
-    //parte_ant = linha.parte;
-    //modulo_ant = linha.modulo;
+    parte_ant = linha[3];
+    modulo_ant = linha[2];
     painel_ant = linha[0];//.painel;
+    
+    if ((paineis % 2) == 0){
+      claro = cor_claro;
+    }
+    else {
+      claro = cor_escuro;
+    }
     
     y++;
   }
