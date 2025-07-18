49e13b02ab68c9d32382ba75366b8795ddaa619e
Ezequiel <ezeq.cruel@gmail.com>
2025-07-18 16:50:25 -0300

    Add files via upload

---
+++ ssd/cz_proj/_dados/projeto.lua
@@ -0,0 +1,16 @@
+-- PELICAnU - Projeto Elétrico, Lógico, Interligação, Controle, Automação & Unifilar
+-- Autor: Ezequiel Rabelo de Aguiar - 2023
+-- Arquivo de configuracao de um projeto no PELICAnU, criado automaticamente
+-- Utiliza a sintaxe padrao da linguagem Lua 5.4. Codificado em UTF-8
+-- ========!! Altere-o com parcimonia !!========
+
+titulo = "Projeto Teste"
+instalacao = "SE Modelo"
+aplicacao = "22999"
+codigo = "MODU"
+rev = "a"
+aprovacao = "ERA-55897"
+visto = "CEFN"
+projetista = "ZEC"
+descr = "Projeto teste"
+data = "10/07/2025"
